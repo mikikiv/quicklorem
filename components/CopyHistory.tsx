d499345b97ee4f705a2a50e9a86b4dafe5c91609
Mikey <46366395+mikikiv@users.noreply.github.com>
2023-07-02 21:17:00 -0700

    Able to read copy history

--- components/CopyHistory.tsx
+++ components/CopyHistory.tsx
@@ -1,40 +1,53 @@
 import { Button, CopyButton, Text, Timeline, Title } from "@mantine/core"
 import { IconCopy } from "@tabler/icons-react"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 type Props = {}
+
+type CopyHistory = {
+  id: string
+  value: string
+}
 
 export default function CopyHistory({}: Props) {
   const [selectedHistory, setSelectedHistory] = useState<number>(0)
 
+  const [copyHistory, setCopyHistory] = useState<CopyHistory[] | null>([])
+
+  useEffect(() => {
+    const copyHistory = localStorage.getItem("copyHistory")
+    if (copyHistory !== null) {
+      setCopyHistory(JSON.parse(copyHistory))
+    }
+  }, [])
+
   return (
     <>
       <Timeline bulletSize={24}>
-        <Timeline.Item
-          bullet={<IconCopy cursor="pointer" size={16} />}
-          key={1}
-          onClick={() => {
-            setSelectedHistory(1)
-          }}
-          active={selectedHistory === 1}
-          style={{ cursor: "pointer" }}
-        >
-          <CopyButton
-            timeout={2000}
-            value={"this is copy text that is too long to display "}
+        {copyHistory?.map((copyItem) => (
+          <Timeline.Item
+            bullet={<IconCopy cursor="pointer" size={16} />}
+            key={copyItem.id}
+            onClick={() => {
+              setSelectedHistory(1)
+            }}
+            active={selectedHistory === 1}
+            style={{ cursor: "pointer" }}
           >
-            {({ copied, copy }) => (
-              <Text
-                onClick={copy}
-                lineClamp={selectedHistory !== 1 ? 1 : undefined}
-              >
-                {copied
-                  ? "copied this is the copy textthat is too long to display"
-                  : "this is the copy textthat is too long to display"}
-              </Text>
-            )}
-          </CopyButton>
-        </Timeline.Item>
+            <CopyButton timeout={2000} value={copyItem.value}>
+              {({ copied, copy }) => (
+                <Text
+                  onClick={copy}
+                  lineClamp={selectedHistory !== 1 ? 1 : undefined}
+                >
+                  {copied
+                    ? "copied this is the copy textthat is too long to display"
+                    : copyItem.value}
+                </Text>
+              )}
+            </CopyButton>
+          </Timeline.Item>
+        ))}
       </Timeline>
     </>
   )
